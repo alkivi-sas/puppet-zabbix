3a3d69fb281a191fdbc4acd1793fb85c6c416781
Alkivi <admin@alkivi.fr>
2015-03-24 13:01:18 +0100

    move to zabbix 2.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class zabbix () {
   # Define apt reposity
   apt::source { 'zabbix':
-    location   => 'http://repo.zabbix.com/zabbix/2.2/debian',
+    location   => 'http://repo.zabbix.com/zabbix/2.4/debian',
     repos      => 'main',
     release    => 'wheezy',
     key        => '79EA5ED4',
